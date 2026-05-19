caf6fa5a9e44a2de14723a58d9ded8daea0f9f6f
Blue-deep95 <bluedeep95@outlook.com>
2026-05-19 12:31:59 +0530

    worked on basic messaging for sending recieving messages also implemented sending messages to the frontend thruogh web-sockets if the user is wathcing the same room or group window . moved explainations to a seperate folder outside the frontend and backend

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 // takes user as an input and returns a signed jwt token using .env files
 const generateAccessToken = (user)=>{
-    return jwt.sign({id:user._id,},process.env.JWT_ACCESS_TOKEN,{expiresIn:'30m'})
+    return jwt.sign({id:user._id,},process.env.JWT_ACCESS_TOKEN,{expiresIn:'15m'})
 }
 
 
